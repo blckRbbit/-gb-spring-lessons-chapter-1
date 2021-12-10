76749d81b380ca7fada29d559b8e05dfbf60b7e7
blckRbbit <lex.leonidov@gmail.com>
2021-12-10 17:11:55 +0300

    added pagination to frontend

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,6 +1,8 @@
 angular.module('app', []).controller('productController', function ($scope, $http) {
      const contextPath = 'http://localhost:8187/app/api/v1';
+     let currentPageIndex = 1;
      $scope.loadProducts = function (pageIndex = 1) {
+     currentPageIndex = pageIndex;
             $http({
                 method: 'GET',
                 url: contextPath + '/products',
@@ -8,19 +10,30 @@ angular.module('app', []).controller('productController', function ($scope, $htt
                     min_cost: $scope.filter ? $scope.filter.min_cost : null,
                     max_cost: $scope.filter ? $scope.filter.max_cost : null,
                     id: $scope.filter ? $scope.filter.id : null,
-                    title: $scope.filter ? $scope.filter.title : null
+                    title: $scope.filter ? $scope.filter.title : null,
+                    page: pageIndex
                 }
             }).then(function(response) {
-                console.log(response.data)
-                $scope.ProductsPage = response.data.content;
+                $scope.pages = response.data;
+                $scope.productsPage = response.data.content;
+                $scope.paginationArray = $scope.generatePagesIndexes(1, $scope.pages.totalPages);
             });
         };
 
     $scope.deleteProduct = function (id) {
         $http.delete(contextPath + '/products/' + id)
             .then(function (response) {
-                $scope.loadProducts();
+                $scope.loadProducts(currentPageIndex);
             });
     }
+
+    $scope.generatePagesIndexes = function(startPage, endPage) {
+        let pages = [];
+        for (let i = startPage; i < endPage + 1; i++) {
+            pages.push(i);
+        }
+        return pages;
+    }
+
     $scope.loadProducts();
 });
